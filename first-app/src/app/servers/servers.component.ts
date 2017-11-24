0d2ca9d86e877531afefc41a20da5438f341c57b
davidokun <davidokun99@gmail.com>
2017-11-24 09:13:44 -0500

    Adding Event Binding sample.

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
 
   constructor() {
     setTimeout(() => {
@@ -17,6 +18,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
   }
 
 }
