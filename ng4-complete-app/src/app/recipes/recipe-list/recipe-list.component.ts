cd809eae99aeb4512c280fdf3e27826f3eeb4be0
davidokun <davidokun99@gmail.com>
2017-12-19 09:02:27 -0500

    ng4 - Update Pizza image to fix display broken.

--- ng4-complete-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import {RecipeModel} from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: RecipeModel[] = [
-    new RecipeModel('Pizza', 'Peperoni Pizza', 'https://pixnio.com/free-images/2017/03/23/2017-03-23-13-45-54.jpg'),
+    new RecipeModel('Pizza', 'Peperoni Pizza', 'https://c1.staticflickr.com/5/4097/4932649560_807c7382e4_b.jpg'),
     new RecipeModel('Hamburger', 'Hamburger with meat and salad', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAiJasapoNK5KqZJnU1I9a9r3Hd1mwCAoSI-h-vtJj5aRfJu2M'),
     new RecipeModel('Soup', 'Vegetables Soup', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF71o_RTMl1PKU8g1s_ofEwTgd04jtcTT21SLX0aRbXpRMNZXXnQ')
   ];
