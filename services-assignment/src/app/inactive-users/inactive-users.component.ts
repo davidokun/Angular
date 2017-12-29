e897a5922473a1e2a8f4673579915f6c6b2e5d8c
davidokun <davidokun99@gmail.com>
2017-12-29 09:33:07 -0500

    services - Update to remove passing array to components and using ths one on UserService directly.

--- services-assignment/src/app/inactive-users/inactive-users.component.ts
+++ services-assignment/src/app/inactive-users/inactive-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from '../services/user.service';
 
 @Component({
@@ -6,12 +6,16 @@ import {UserService} from '../services/user.service';
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
+export class InactiveUsersComponent implements OnInit{
+  users: string[];
 
   constructor(private userService: UserService) {}
 
   onSetToActive(id: number) {
     this.userService.setToActive(id);
+  }
+
+  ngOnInit(): void {
+    this.users = this.userService.inactiveUsers;
   }
 }
