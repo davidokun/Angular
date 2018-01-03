bb7b64c760980e27d9b3ebd277206841fa42366f
davidokun <davidokun99@gmail.com>
2018-01-03 08:27:05 -0500

    ng4 - Adding Ingredients to Recipes.

--- ng4-complete-app/src/app/recipes/recipe.service.ts
+++ ng4-complete-app/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {RecipeModel} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {IngredientModel} from "../shared/ingredient.model";
 
 export class RecipeService {
 
@@ -8,13 +9,16 @@ export class RecipeService {
   private recipes: RecipeModel[] = [
     new RecipeModel('Pizza',
       'Peperoni Pizza',
-      'https://c1.staticflickr.com/5/4097/4932649560_807c7382e4_b.jpg'),
+      'https://c1.staticflickr.com/5/4097/4932649560_807c7382e4_b.jpg',
+      [new IngredientModel('Cheese', 1), new IngredientModel('Peperoni', 8)]),
     new RecipeModel('Hamburger',
       'Hamburger with meat and salad',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAiJasapoNK5KqZJnU1I9a9r3Hd1mwCAoSI-h-vtJj5aRfJu2M'),
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAiJasapoNK5KqZJnU1I9a9r3Hd1mwCAoSI-h-vtJj5aRfJu2M',
+      [new IngredientModel('Meat', 1), new IngredientModel('Bread', 2)]),
     new RecipeModel('Soup',
       'Vegetables Soup',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF71o_RTMl1PKU8g1s_ofEwTgd04jtcTT21SLX0aRbXpRMNZXXnQ')
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF71o_RTMl1PKU8g1s_ofEwTgd04jtcTT21SLX0aRbXpRMNZXXnQ',
+      [new IngredientModel('Tomato', 1), new IngredientModel('Garlic', 4)])
   ];
 
   getRecipes() {
