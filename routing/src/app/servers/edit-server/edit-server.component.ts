72c4b71cbdb753ecf41df8e50ca4b02daa3b2269
davidokun <davidokun99@gmail.com>
2018-01-10 09:04:02 -0500

    routing - Using Query Parameters - Practice.

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) { }
@@ -22,12 +23,21 @@ export class EditServerComponent implements OnInit {
     console.log(this.route.snapshot.fragment);
 
     // Subscribe to any param or fragment even if the component is detroyed.
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    );
     this.route.fragment.subscribe();
 
-    this.server = this.serversService.getServer(1);
-    this.serverName = this.server.name;
-    this.serverStatus = this.server.status;
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+        this.serverName = this.server.name;
+        this.serverStatus = this.server.status;
+      }
+    );
+
   }
 
   onUpdateServer() {
