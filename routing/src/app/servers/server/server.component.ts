cf24439b3bcf2fb02ddcd13ae024527ae82cd714
davidokun <davidokun99@gmail.com>
2018-01-12 08:53:03 -0500

    routing - Resolving Dynamic Data with the resolve Guard.

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ActivatedRoute, Data, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -16,15 +16,23 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    // Using '+' sing to convert a string param to number.
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
 
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
+    // Using the ServerResolver guard to fetch the server
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
       }
     );
+
+    // // Using '+' sing to convert a string param to number.
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
+    //
+    // this.route.params.subscribe(
+    //   (params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   }
+    // );
 
   }
 
