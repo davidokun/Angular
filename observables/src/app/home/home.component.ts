19099988f82cda6cabc66ea2a0829cf614a588d1
davidokun <davidokun99@gmail.com>
2018-01-17 08:40:50 -0500

    observables - Unsubscribe observables.

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,26 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
 import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  numberObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() {
   }
 
   ngOnInit() {
-    // const myNumbers = Observable.interval(1000);
-    //
-    // myNumbers.subscribe(
-    //   (number: number) => {
-    //     console.log(number);
-    //   }
-    // );
+    const myNumbers = Observable.interval(1000);
+
+    this.numberObsSubscription = myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
 
     const myObservable = Observable.create((observer: Observer<string>) => {
       setTimeout(() => {
@@ -38,12 +42,17 @@ export class HomeComponent implements OnInit {
       }, 6000);
     });
 
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: string) => { console.log(data); }, // First parameter, callback function.
       (error: string) => { console.log(error); }, // Second parameter, handling error.
       () => { console.log('Completed!'); } // Third parameter, handling completed.
 
     );
+  }
+
+  ngOnDestroy(): void {
+    this.numberObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 
 }
