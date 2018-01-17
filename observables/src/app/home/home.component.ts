b7bccedf4e3ef45446da342089e6cc3c0a7ded80
davidokun <davidokun99@gmail.com>
2018-01-17 08:32:04 -0500

    observables - Building & Using a Custom Observable from Scratch.

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,35 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
+    // const myNumbers = Observable.interval(1000);
+    //
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
 
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
-      }
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('First package'); // Push the next data package
+      }, 2000);
+      setTimeout(() => {
+        observer.next('Second package'); // Push the next data package
+      }, 4000);
+      setTimeout(() => {
+        observer.error('This does not work'); // Push the next data package
+        observer.complete(); // Finish sending data
+      }, 5000);
+      setTimeout(() => {
+        observer.next('Third package'); // Push the next data package
+      }, 6000);
+    });
+
+    myObservable.subscribe(
+      (data: string) => { console.log(data); }, // First parameter, callback function.
+      (error: string) => { console.log(error); }, // Second parameter, handling error.
+      () => { console.log('Completed!'); } // Third parameter, handling completed.
+
     );
   }
 
