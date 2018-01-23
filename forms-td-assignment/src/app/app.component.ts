4e8204bb1d45944ad80eb65af325ed1ca28e94a6
davidokun <davidokun99@gmail.com>
2018-01-23 09:50:08 -0500

    forms-td-assignment - Implementation of Form.

--- forms-td-assignment/src/app/app.component.ts
+++ forms-td-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('frm') dataForm: NgForm;
+  defaultSubscription: 'Advance';
+  subscriptions = ['Basic', 'Advance', 'Pro'];
+  submitted = false;
+  data = {
+    email: '',
+    subs: '',
+    pass: ''
+  };
+
+  onFormSubmit() {
+    this.submitted = true;
+
+    this.data.email = this.dataForm.value.email;
+    this.data.subs = this.dataForm.value.subs;
+    this.data.pass = this.dataForm.value.pass;
+
+    this.dataForm.reset();
+  }
 }
