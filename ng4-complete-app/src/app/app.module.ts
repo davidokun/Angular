26fdf4c4907625a1547b111c98c80c4990db03fe
davidokun <davidokun99@gmail.com>
2018-02-05 08:51:23 -0500

    ng4 - Providing the Recipe Service Correctly.

--- ng4-complete-app/src/app/app.module.ts
+++ ng4-complete-app/src/app/app.module.ts
@@ -16,7 +16,8 @@ import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RecipeService} from './recipes/recipe.service';
 
 
 @NgModule({
@@ -40,7 +41,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
