cbf1b83852652d945a3bb6a7da40b84cf49c319a
davidokun <davidokun99@gmail.com>
2018-02-09 08:54:04 -0500

    ng4 - GETting Back the Recipes.

--- ng4-complete-app/src/app/recipes/recipe.service.ts
+++ ng4-complete-app/src/app/recipes/recipe.service.ts
@@ -52,4 +52,9 @@ export class RecipeService {
     this.recipes.splice(index, 1);
     this.recipesChange.next(this.recipes.slice());
   }
+
+  setRecipes(recipes: RecipeModel[]) {
+    this.recipes = recipes;
+    this.recipesChange.next(this.recipes.slice());
+  }
 }
--- ng4-complete-app/src/app/shared/data-storage.service.ts
+++ ng4-complete-app/src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
+import {RecipeModel} from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -11,5 +12,15 @@ export class DataStorageService {
   storeRecipes() {
     return this.http.put('https://ng-recipe-book-a72d0.firebaseio.com/recipes.json',
       this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book-a72d0.firebaseio.com/recipes.json')
+      .subscribe(
+        (response: Response) => {
+          const recipes: RecipeModel[] = response.json();
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
