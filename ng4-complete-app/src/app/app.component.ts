3df3444de3a91c6840bf6a937a21fa75de16ae15
davidokun <davidokun99@gmail.com>
2018-02-12 08:59:29 -0500

    ng4 - Setting up the Firebase SDK.

--- ng4-complete-app/src/app/app.component.ts
+++ ng4-complete-app/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDqTkQ3JSBzLSJN5kfzSamVosLytjkUo3s',
+      authDomain: 'ng-recipe-book-a72d0.firebaseapp.com'
+    });
   }
 }
