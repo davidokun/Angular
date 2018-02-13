ab02ea3576e5c2bf6873955710bba8009342af07
davidokun <davidokun99@gmail.com>
2018-02-13 09:10:58 -0500

    ng4 - Checking and Using Authentication Status.

--- ng4-complete-app/src/app/header/header.component.ts
+++ ng4-complete-app/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataStorageService} from '../shared/data-storage.service';
 import {Response} from '@angular/http';
+import {AuthService} from "../auth/auth.service";
 
 @Component ({
   selector: 'app-header',
@@ -12,7 +13,8 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  constructor (private dataStorageService: DataStorageService) {
+  constructor (private dataStorageService: DataStorageService,
+               private authService: AuthService) {
   }
 
   onSaveData() {
