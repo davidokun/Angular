35366b06cf46c94404ee641cbd726598977fb4b3
davidokun <davidokun99@gmail.com>
2018-02-13 08:39:42 -0500

    ng4 - Signing Users In.

--- ng4-complete-app/src/app/auth/auth.service.ts
+++ ng4-complete-app/src/app/auth/auth.service.ts
@@ -2,9 +2,19 @@ import * as firebase from 'firebase';
 
 export class AuthService {
 
-  signupUser(email: string, password: string) {
+  signUpUser(email: string, password: string) {
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log(error)
+      );
+  }
+
+  signInUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => console.log(response)
+      )
       .catch(
         error => console.log(error)
       );
