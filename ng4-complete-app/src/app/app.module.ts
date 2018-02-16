f0e87f0fcdbc3ac2545664ccbb44a8b3f2c40d66
davidokun <davidokun99@gmail.com>
2018-02-16 08:40:24 -0500

    ng4-modules - Creating the Auth Feature Module.

--- ng4-complete-app/src/app/app.module.ts
+++ ng4-complete-app/src/app/app.module.ts
@@ -5,27 +5,22 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ImproveDropdownDirective} from './shared/improve-dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
 import {DataStorageService} from './shared/data-storage.service';
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
 import {AuthService} from './auth/auth.service';
 import {AuthGuardService} from './auth/auth-guard.service';
 import {RecipesModule} from './recipes/recipes.module';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
+import {AuthModule} from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ImproveDropdownDirective,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +28,8 @@ import {ShoppingListModule} from './shopping-list/shopping-list.module';
     HttpModule,
     RecipesModule,
     SharedModule,
-    ShoppingListModule
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
--- ng4-complete-app/src/app/shared/shared.module.ts
+++ ng4-complete-app/src/app/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import {NgModule} from '@angular/core';
 import {DropdownDirective} from './dropdown.directive';
 import {CommonModule} from '@angular/common';
+import {ImproveDropdownDirective} from './improve-dropdown.directive';
 
 @NgModule({
   declarations: [
-    DropdownDirective
+    DropdownDirective,
+    ImproveDropdownDirective
   ],
   exports: [
     CommonModule,
-    DropdownDirective
+    DropdownDirective,
+    ImproveDropdownDirective
   ]
 })
 export class SharedModule {}
