feac0d2fd5dc0239f1bedd7d91b54c51107c7c19
davidokun <davidokun99@gmail.com>
2018-02-16 09:41:17 -0500

    ng4-modules - Creating a Basic Core Module.

--- ng4-complete-app/src/app/app.module.ts
+++ ng4-complete-app/src/app/app.module.ts
@@ -4,7 +4,6 @@ import {HttpModule} from '@angular/http';
 
 
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
@@ -14,14 +13,12 @@ import {AuthGuardService} from './auth/auth-guard.service';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
-import {HomeComponent} from './home/home.component';
+import {CoreModule} from './core/core.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +26,8 @@ import {HomeComponent} from './home/home.component';
     HttpModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
+    AuthModule,
+    CoreModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
