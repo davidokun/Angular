4c94dae7bbea1efca8aa445327f4e09426086c3e
davidokun <davidokun99@gmail.com>
2018-02-19 10:07:01 -0500

    ng4-new-http - Requesting Events.

--- ng4-complete-app/src/app/core/header/header.component.ts
+++ ng4-complete-app/src/app/core/header/header.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataStorageService} from '../../shared/data-storage.service';
-import {Response} from '@angular/http';
 import {AuthService} from '../../auth/auth.service';
+import {HttpEvent} from '@angular/common/http';
 
 @Component ({
   selector: 'app-header',
@@ -20,8 +20,10 @@ export class HeaderComponent implements OnInit {
   onSaveData() {
     this.dataStorageService.storeRecipes()
       .subscribe(
-        (response: Response) => {
+        (response: HttpEvent<Object>) => {
           console.log(response);
+          // console.log(response.type === HttpEventType.Sent);
+          // console.log(response.type === HttpEventType.Response);
         }
       );
   }
--- ng4-complete-app/src/app/shared/data-storage.service.ts
+++ ng4-complete-app/src/app/shared/data-storage.service.ts
@@ -15,7 +15,10 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
     return this.httpClient.put('https://ng-recipe-book-a72d0.firebaseio.com/recipes.json?auth=' + token,
-      this.recipeService.getRecipes());
+      this.recipeService.getRecipes(), {
+        // observe: 'events'
+        observe: 'body'
+      });
   }
 
   getRecipes() {
