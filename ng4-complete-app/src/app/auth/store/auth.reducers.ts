9618a4358ef6d8eb59c5c6ef8a96a94cb5fc4ed8
davidokun <davidokun99@gmail.com>
2018-02-23 10:08:40 -0500

    ngrx - Handling the Auth Token.

--- ng4-complete-app/src/app/auth/store/auth.reducers.ts
+++ ng4-complete-app/src/app/auth/store/auth.reducers.ts
@@ -27,6 +27,12 @@ export function authReducer(state = initialState, action: AuthActions.AuthAction
         authenticated: false
       };
 
+    case (AuthActions.SET_TOKEN):
+      return {
+        ...state,
+        token: action.payload
+      };
+
     default:
       return state;
   }
