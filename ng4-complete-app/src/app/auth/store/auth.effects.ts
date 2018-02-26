627b3b74d23f8a8259e626b10318eba261454ca5
davidokun <davidokun99@gmail.com>
2018-02-26 08:57:56 -0500

    ngrx - Auth Effects and Actions.

--- ng4-complete-app/src/app/auth/store/auth.effects.ts
+++ ng4-complete-app/src/app/auth/store/auth.effects.ts
@@ -1,7 +1,11 @@
-import {Effect} from '@ngrx/effects';
+import {Actions, Effect} from '@ngrx/effects';
+import {Injectable} from '@angular/core';
 
+@Injectable()
 export class AuthEffects {
 
   @Effect()
   authSignUp;
+
+  constructor(private actions$: Actions) {}
 }
