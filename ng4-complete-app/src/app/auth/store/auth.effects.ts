a12cba740476b84109a6b8c10cc2a771ee1c90e3
davidokun <davidokun99@gmail.com>
2018-02-28 08:31:24 -0500

    ngrx - Adding Auth SignUp.

--- ng4-complete-app/src/app/auth/store/auth.effects.ts
+++ ng4-complete-app/src/app/auth/store/auth.effects.ts
@@ -1,13 +1,38 @@
 import {Actions, Effect} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
 import * as AuthActions from './auth.actions';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import * as firebase from 'firebase';
+import {fromPromise} from 'rxjs/observable/fromPromise';
 
 @Injectable()
 export class AuthEffects {
 
   @Effect()
   authSignUp = this.actions$
-    .ofType(AuthActions.TRY_SIGN_UP);
+    .ofType(AuthActions.TRY_SIGN_UP)
+    .map((action: AuthActions.TrySignUp) => {
+      return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+      return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      return [
+        {
+          type: AuthActions.SIGN_UP
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions) {
+  }
 }
