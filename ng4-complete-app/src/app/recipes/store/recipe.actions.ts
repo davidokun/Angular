7c882ba14b18db7376e8b80370c5ef64891138cc
davidokun <davidokun99@gmail.com>
2018-03-01 18:50:30 -0500

    ngrx - Adding Recipe Actions.

---
+++ ng4-complete-app/src/app/recipes/store/recipe.actions.ts
@@ -0,0 +1,34 @@
+import {Action} from '@ngrx/store';
+import {RecipesModule} from '../recipes.module';
+import {RecipeModel} from '../recipe.model';
+
+export const SET_RECIPES = 'SET_RECIPES';
+export const ADD_RECIPES = 'ADD_RECIPES';
+export const UPDATE_RECIPES = 'UPDATE_RECIPES';
+export const DELETE_RECIPES = 'DELETE_RECIPES';
+
+export class SetRecipes implements Action {
+  readonly type = SET_RECIPES;
+
+  constructor(public payload: RecipesModule[]) {}
+}
+
+export class AddRecipe implements Action {
+  readonly type = ADD_RECIPES;
+
+  constructor(public payload: RecipesModule) {}
+}
+
+export class UpdateRecipes implements Action {
+  readonly type = UPDATE_RECIPES;
+
+  constructor(public payload: {index: number, updateRecipe: RecipeModel}) {}
+}
+
+export class DeleteRecipes implements Action {
+  readonly type = DELETE_RECIPES;
+
+  constructor(public payload: number) {}
+}
+
+export type RecipeActions = SetRecipes | AddRecipe | UpdateRecipes | DeleteRecipes;
