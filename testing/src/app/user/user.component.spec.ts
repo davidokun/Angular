b13ad6dc5aa3bd7193116f75739b7a48c19ead74
davidokun <davidokun99@gmail.com>
2018-03-08 15:28:06 -0500

    testing - Adding a Component and some fitting Tests.

---
+++ testing/src/app/user/user.component.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserComponent } from './user.component';
+
+describe('UserComponent', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserComponent]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(UserComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  // let component: UserComponent;
+  // let fixture: ComponentFixture<UserComponent>;
+  //
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ UserComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
+  //
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(UserComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+  //
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+});
