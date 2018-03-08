379dbf6baad757731b5d604ecd876d2a1e27676c
davidokun <davidokun99@gmail.com>
2018-03-08 17:11:53 -0500

    testing - Simulating Async Tasks.

--- testing/src/app/user/user.component.spec.ts
+++ testing/src/app/user/user.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
 import {UserService} from './user.service';
+import {DataService} from '../shared/data.service';
 
 describe('UserComponent', () => {
 
@@ -41,15 +42,38 @@ describe('UserComponent', () => {
     });
   });
 
-  it('shouldn\'t display the user name if user is logged in', () => {
+  it('shouldn\'t display the user name if user is not logged in', () => {
     const fixture = TestBed.createComponent(UserComponent);
     const app = fixture.debugElement.componentInstance;
     fixture.autoDetectChanges();
     const compiled = fixture.debugElement.nativeElement;
 
     fixture.whenStable().then(() => {
-      !expect(compiled.querySelector('p').textContent).toContain(app.user.name);
+      expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
     });
   });
+
+  it('shouldn\'t fetch data successfully if not called asynchronously', () => {
+    const fixture = TestBed.createComponent(UserComponent);
+    const app = fixture.debugElement.componentInstance;
+    const dataService = fixture.debugElement.injector.get(DataService);
+    const spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Data'));
+    fixture.detectChanges();
+    expect(app.data).toBe(undefined);
+  });
+
+  it('should fetch data successfully if not called asynchronously', async(() => {
+    const fixture = TestBed.createComponent(UserComponent);
+    const app = fixture.debugElement.componentInstance;
+    const dataService = fixture.debugElement.injector.get(DataService);
+    const spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Data'));
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(app.data).toBe('Data');
+    });
+  }));
 
 });
